fada259f083433b196a83d279510ca1dafded396
Andrel02 <andreeesantos25@gmail.com>
2025-01-30 01:13:05 +0000

    Inserção de dados na tabela salario_classe_oficial

--- exercicios/1-CRUD/3-inserir-dados.sql
+++ exercicios/1-CRUD/3-inserir-dados.sql
@@ -1,4 +1,11 @@
--- Insira 3 classes salariais na tabela salario_classe
-
-
+-- Insira 4 classes salariais na tabela salario_classe
+INSERT INTO salario_classe_oficial(id,nivel,salario)
+VALUES
+(1,'estagiária',650),
+(2,'assistente',2000),
+(3,'analista',3500),
+(4,'especialista',8000);
 -- Insira 1 nova classe salarial, sem declarar o atributo id_salario
+INSERT INTO salario_classe_oficial(salario, nivel)
+VALUES
+(10000, 'gerente');
