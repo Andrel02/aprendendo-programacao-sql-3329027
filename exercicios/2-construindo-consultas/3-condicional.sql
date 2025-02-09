718e0dc37d1f48453e5f33c18e10f135aa5a6019
Andrel02 <andreeesantos25@gmail.com>
2025-02-09 01:23:53 +0000

    Estados brasileiros por extenso

--- exercicios/2-construindo-consultas/3-condicional.sql
+++ exercicios/2-construindo-consultas/3-condicional.sql
@@ -1,2 +1,11 @@
 -- Escreva uma consulta que retorne o nome por extenso dos estados brasileiros
-
+SELECT DISTINCT
+Country as pais,
+State as estado_sigla,
+CASE
+    WHEN state = 'SP' THEN 'São Paulo'
+    WHEN state = 'RJ' THEN 'Rio de Janeiro'
+    WHEN state = 'DF' THEN 'Distrito Federal'
+END as estado
+FROM customers
+WHERE Country = 'Brazil';
